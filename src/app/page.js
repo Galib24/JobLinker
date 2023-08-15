e2294dd50a53774160f26ab406b3fe971ee51082
SQLAdmin <abuyeahia24@gmail.com>
2023-08-15 22:21:41 +0600

    added feature job data of HR section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import HomeBanner from "@/components/HomePage/Banner/HomeBanner";
+import FeaturedJobs from "@/components/HomePage/FeaturedJobs/FeaturedJobs";
 import JobCategories from "@/components/HomePage/JobCategories/JobCategories";
 import JobsBanner from "@/components/HomePage/JobsBanner/JobsBanner";
 import Image from "next/image";
@@ -11,6 +12,9 @@ export default function Home() {
 
       {/* Popular job category section */}
       <JobCategories></JobCategories>
+
+      {/* feature job section */}
+      <FeaturedJobs></FeaturedJobs>
 
       {/* Jobs Banner section */}
       <JobsBanner></JobsBanner>
---
+++ src/components/HomePage/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import dataOfFeaturedJobs from '@/Data/featuredJobs';
+
+
+const FeaturedJobs = () => {
+    // console.log(dataOfFeaturedJobs);
+   const topFeaturedJobs  = dataOfFeaturedJobs.filter(i => i.rating >= 5)
+   console.log(topFeaturedJobs);
+    return (
+        <div>
+            <h2>feature job: {dataOfFeaturedJobs.length}</h2>
+            <h2>{topFeaturedJobs.length}</h2>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
