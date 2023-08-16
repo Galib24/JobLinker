e684cace6126618856c2d7a05692a8a0a9b03d98
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-08-16 19:12:58 +0600

    add testimoanil section by shakil

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
 
       {/* Jobs Banner section */}
       <JobsBanner></JobsBanner>
+
       {/* Testimonial section */}
       <TestimonialSection></TestimonialSection>
     </div>
