1ef557c9f2221661e755963b601fb33ed204e681
arafat-sani <ffsani029@gmail.com>
2023-08-21 18:54:24 +0600

    blog section responsive

---
+++ src/Data/blog.js
@@ -0,0 +1,45 @@
+const dataOfBlog = [
+    {
+      id: "1",
+      image: "https://i.ibb.co/p4y2K1s/blog1.jpg",
+      authorImage: "https://i.ibb.co/2krh2m4/author1.jpg",
+      name: "Addison Kobe",
+      jobRole: "Web developer",
+      jobDescription: "The success of every websites now depends on search engine optimisation and digital marketing strategy..."
+    },
+    {
+      id: "2",
+      image: "https://i.ibb.co/MnYpmKG/blog2.webp",
+      authorImage: "https://i.ibb.co/NF54Zjv/author2.jpg",
+      name: "Mathias Henry",
+      jobRole: "Graphic Designer",
+      jobDescription: "Creative graphic designer with a keen eye for detail and a love for visual storytelling."
+    },
+    {
+      id: "3",
+      image: "https://i.ibb.co/LRHzYWc/blog3.jpg",
+      authorImage: "https://i.ibb.co/QYmJ0jV/author3.jpg",
+      name: "Tristin Eli",
+      jobRole: "Marketing Manager",
+      jobDescription: "Strategic marketing manager with a track record of driving successful campaigns and engaging customers."
+    },
+    {
+        id: "4",
+        image: "https://i.ibb.co/BLmhwSW/blog4.jpg",
+        authorImage: "https://i.ibb.co/b2nTCWH/author4.jpg",
+        name: "Mary R. Phillips",
+        jobRole: "Data Scientist",
+        jobDescription: "Passionate data scientist with expertise in analyzing complex datasets and deriving actionable insights."
+    },
+    {
+        id: "5",
+        image: "https://i.ibb.co/Bg8xT4g/blog5.jpg",
+        authorImage: "https://i.ibb.co/z7z4M5S/author5.jpg",
+        name: "Steven D. Wheeler",
+        jobRole: "Financial Analyst",
+        jobDescription: "Experienced financial analyst with a knack for interpreting financial trends and making strategic recommendations."
+    }
+  ]
+
+  export default dataOfBlog;
+  
--- src/components/HomePage/Blog/Blog.jsx
+++ src/components/HomePage/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 'use client'
+import dataOfBlog from "@/Data/blog"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -11,19 +12,6 @@ import { Pagination } from 'swiper/modules';
 import Image from "next/image";
 
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
-
-import bImg1 from "@/asserts/blogs/blog1.jpeg";
-import bImg2 from "@/asserts/blogs/blog2.webp";
-import bImg3 from "@/asserts/blogs/blog3.jpg";
-import bImg4 from "@/asserts/blogs/blog4.jpg";
-import bImg5 from "@/asserts/blogs/blog5.jpg";
-
-import aImg1 from "@/asserts/blog-author/author1.jpg";
-import aImg2 from "@/asserts/blog-author/author2.jpg";
-import aImg3 from "@/asserts/blog-author/author3.jpg";
-import aImg4 from "@/asserts/blog-author/author4.jpg";
-import aImg5 from "@/asserts/blog-author/author5.jpg";
-
 
 const Blog = () => {
     return (
@@ -40,119 +28,48 @@ const Blog = () => {
                     }}
                     modules={[Pagination]}
                     className="mySwiper"
+
+                // responsive breakpoint
+                    breakpoints={{
+                        0:{
+                            slidesPerView:1,
+                            spaceBetween:10,
+                        },
+                        620:{
+                            slidesPerView:2,
+                            spaceBetween:10,
+                        }
+                        ,
+                        840:{
+                            slidesPerView:3,
+                            spaceBetween:20,
+                        }
+                    }}
                 >
-                    <SwiperSlide>
-                        <Image src={bImg1} alt=''></Image>
-                        <div>
-                            <div className='flex items-center  mb-4'>
-                                <div>
-                                    <Image className=' rounded-full  mt-5 w-12' src={aImg1} alt=''></Image>
+                        {
+                        dataOfBlog.map(item =>  <SwiperSlide  key={item.id}>
+                            <Image width={500} height={500}  src={item?.image} alt=''></Image>
+                            <div>
+                                <div className='flex items-center  mb-4'>
+                                    <div>
+                                        <Image className='rounded-full mt-5' width={40} height={40} src={item?.authorImage} alt=''></Image>
+                                    </div>
+                                    <div className='ml-2'>
+                                        <h1 className=' text-xl font-bold mt-5 '>{item?.name}</h1>
+                                        <FaStar className='  text-amber-300  mr-1'></FaStar>
+                                        <FaStar className=' text-amber-300 mr-1'></FaStar>
+                                        <FaStar className=' text-amber-300 mr-1'></FaStar>
+                                        <FaStar className=' text-amber-300 mr-1'></FaStar>
+                                        <FaStarHalfAlt className=' text-amber-300'></FaStarHalfAlt>
+                                    </div>
                                 </div>
-                                <div className='ml-2'>
-                                    <h1 className=' text-xl font-bold mt-5 '>Addison Kobe</h1>
-                                    <FaStar className='  text-amber-300  mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStarHalfAlt className=' text-amber-300'></FaStarHalfAlt>
-                                </div>
+                                <h2 className=' text-base font-bold'>{item?.jobRole}</h2>
+                                <p className=' w-72  text-zinc-500'>{item?.jobDescription}</p>
+                                <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
                             </div>
-                            <h2 className=' text-base font-bold'>Web developer</h2>
-                            <p className=' w-80  text-zinc-500'>The success of every websites now depends on search engine optimisation and digital marketing strategy...</p>
-                            <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
-                        </div>
 
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Image src={bImg2} alt=''></Image>
-
-                        <div>
-                            <div className='flex items-center  mb-4'>
-                                <div >
-                                    <Image className=' rounded-full  mt-5 w-12' src={aImg2} alt=''></Image>
-                                </div>
-                                <div className='ml-2'>
-                                    <h1 className=' text-xl font-bold mt-5'>Mathias Henry</h1>
-                                    <FaStar className=' text-amber-300  mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300'></FaStar>
-                                </div>
-                            </div>
-                            <h2 className=' text-base font-bold'>Frontend web developer</h2>
-                            <p className=' w-80  text-zinc-500'>If debugging is the process of removing software bugs, then programming must be the process of putting them in...</p>
-                            <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
-                        </div>
-
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Image src={bImg3} alt=''></Image>
-                        <div>
-                            <div className='flex items-center  mb-4'>
-                                <div>
-                                    <Image className=' rounded-full mt-5 w-12' src={aImg3} alt=''></Image>
-                                </div>
-                                <div className='ml-2'>
-                                    <h1 className=' text-xl font-bold mt-5'>Tristin Eli</h1>
-                                    <FaStar className=' text-amber-300  mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStarHalfAlt className=' text-amber-300 mr-1'></FaStarHalfAlt>
-                                    <FaStarHalfAlt className=' text-amber-300'></FaStarHalfAlt>
-                                </div>
-                            </div>
-                            <h2 className=' text-base font-bold'>Data Analytics</h2>
-                            <p className=' w-80  text-zinc-500'>Data are just summaries of thousands of stories—tell a few of those stories to help make the data meaningful...</p>
-                            <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Image src={bImg4} alt=''></Image>
-                        <div>
-                            <div className='flex items-center  mb-4'>
-                                <div>
-                                    <Image className=' rounded-full  mt-5 w-12' src={aImg4} alt=''></Image>
-                                </div>
-                                <div className='ml-2'>
-                                    <h1 className=' text-xl font-bold mt-5'>Mary R. Phillips</h1>
-                                    <FaStar className=' text-amber-300  mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300'></FaStar>
-                                </div>
-                            </div>
-                            <h2 className=' text-base font-bold'>Software Engineer</h2>
-                            <p className=' w-80  text-zinc-500'>Good programmers know what to write. Great ones know what to rewrite and reuse...</p>
-                            <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Image src={bImg5} alt=''></Image>
-                        <div>
-                            <div className='flex items-center mb-4'>
-                                <div>
-                                    <Image className=' rounded-full  mt-5 w-12' src={aImg5} alt=''></Image>
-                                </div>
-                                <div className='ml-2'>
-                                    <h1 className=' text-xl font-bold mt-5'>Steven D. Wheeler</h1>
-                                    <FaStar className=' text-amber-300  mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStar className=' text-amber-300 mr-1'></FaStar>
-                                    <FaStarHalfAlt className=' text-amber-300'></FaStarHalfAlt>
-                                </div>
-                            </div>
-                            <h2 className=' text-base font-bold'>Data Science </h2>
-                            <p className=' w-80  text-zinc-500'>Things get done only if the data we gather can inform and inspire those in a position to make a difference...</p>
-                            <button className='btn  bg-[#40e1f9]  p-3 rounded-lg text-white hover:bg-black hover:text-[#F68519] ease-out duration-300 mt-3'>Read More</button>
-                        </div>
-                    </SwiperSlide>
+                        </SwiperSlide>) 
+                        }
                 </Swiper>
             </div>
         </div>
