235b316833037c5b07467253d0a9109029784883
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-21 16:44:47 +0600

    Added seeker page design

--- src/app/seekers/page.js
+++ src/app/seekers/page.js
@@ -1,11 +1,148 @@
-import React from 'react';
+"use client";
+
+import SingleCard from "@/components/SeekersPage/SingleCard";
+import BannerComponent from "@/components/Shared/BannerComponent/BannerComponent";
+import dataOfJobSeekers from "@/Data/jobSeekers";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const JobSeekersPage = () => {
-    return (
-        <div>
-            <h2 className='mt-40 text-xl font-semibold'>All the job seekers&apos; post here</h2>
+  const [navToggle, setNavToggle] = useState(false);
+
+  return (
+    <div>
+      {/* import banner component for displaying just something, it can be imported from another page */}
+      <BannerComponent
+        headingTitle="Posted Jobs"
+        pageName="Seekers"
+      ></BannerComponent>
+
+      {/* seekers post part start */}
+      <div className="my-20 md:flex">
+        {/* filter by job experience and category */}
+        <div className="md:w-1/3">
+          <div
+            className={` absolute ${
+              navToggle ? "left-0 top-36" : "left-[-120%]"
+            } top-[4.5rem] w-full bg-slate-200 pb-3 pt-2 transition-all duration-300 dark:bg-slate-900 lg:static lg:w-[unset] lg:flex-row lg:bg-transparent lg:pb-0 lg:pt-0 dark:lg:bg-transparent`}
+          >
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text font-semibold text-xl">
+                  Experience Level
+                </span>
+              </label>
+              <select
+                defaultValue="Pick One"
+                className="select focus:outline-[#0B0016]"
+              >
+                <option disabled>Pick One</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+
+            <div className="form-control w-1/2 mt-14">
+              <label className="label">
+                <span className="label-text font-semibold text-xl">
+                  Job Type
+                </span>
+              </label>
+              <select
+                defaultValue="Pick One"
+                className="select focus:outline-[#0B0016]"
+              >
+                <option disabled>Pick One</option>
+                <option>Part Time</option>
+                <option>Full Time</option>
+                <option>Hybrid</option>
+              </select>
+            </div>
+          </div>
         </div>
-    );
+
+        {/* all post here */}
+        <div className="md:w-2/3">
+          {/* filter Toggle */}
+          <div className="my-4 text-center lg:hidden">
+            <div
+              className="swap-rotate swap btn-ghost btn-circle btn ml-2 bg-white dark:bg-slate-800  inline-block"
+              onClick={() => setNavToggle(!navToggle)}
+            >
+              {navToggle ? (
+                // <svg
+                //   className="swap-on fill-current"
+                //   xmlns="http://www.w3.org/2000/svg"
+                //   width="32"
+                //   height="32"
+                //   viewBox="0 0 512 512"
+                // >
+                //   <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                // </svg>
+                <RxCross2 className="text-3xl font-semibold" />
+              ) : (
+                <svg
+                  className="swap-off fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
+              )}
+              <span>Filter</span>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between px-2">
+            <h4 className="">
+              Show{" "}
+              <span className="font-semibold">{dataOfJobSeekers.length}</span>{" "}
+              jobs
+            </h4>
+
+            {/* search field */}
+            <div className="">
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Search by title"
+                  className="input input-bordered rounded-md border-gray-300 focus:outline-[#0B0016] bg-gray-200 text-gray-900"
+                />
+                <button className="btn btn-square">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* Card for showing job post */}
+          <div>
+            {dataOfJobSeekers.map((jobPost) => (
+              <SingleCard key={jobPost._id} jobPost={jobPost}></SingleCard>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobSeekersPage;
--- src/components/HomePage/JobSeekers/SingleJobSeekers.jsx
+++ src/components/HomePage/JobSeekers/SingleJobSeekers.jsx
@@ -3,74 +3,65 @@ import { PiShootingStarFill } from "react-icons/pi";
 import { GiTrophy } from "react-icons/gi";
 
 const SingleJobSeekers = ({ data }) => {
-    const { seekerName, image, jobExperience, education, skills, findingJobTitle, seekerJobTime } = data;
-    // console.log(data);
+  const {
+    seekerName,
+    image,
+    jobExperience,
+    education,
+    skills,
+    findingJobTitle,
+    seekerJobTime,
+  } = data;
+  // console.log(data);
 
-    return (
-        <div
-            className="card border  border-gray-200 bg-base-100 shadow-sm mx-3 my-2">
-            <div
-                className="mt-5 mx-3 flex justify-end">
-                <h2
-                    className="justify-center px-6 py-1 font-light bg-yellow-400 rounded-full text-white text-xs w-[35%] lg:w-[28%] text-center">
-                    {seekerJobTime}
-                </h2>
-            </div>
-            <figure>
-                <Image
-                    width={160}
-                    height={160}
-                    src={image}
-                    className="rounded-full my-3"
-                    alt="Seekers Image"
-                />
-            </figure>
-            <div>
-                <h2
-                    className="text-center font-bold my-1 text-xl">
-                    {seekerName}
-                </h2>
-                <p
-                    className="text-center font-medium text-sm">
-                    {findingJobTitle}
-                </p>
-                <p
-                    className="text-center font-medium text-sm py-1">
-                    Technical: {education}
-                </p>
-            </div>
-            <div
-                className="mb-2">
-                <h2
-                    className="text-center font-bold">
-                    Experience: {jobExperience} <span
-                        className="text-xs">Year</span><PiShootingStarFill className="text-xl text-yellow-400 ms-1" />
-                </h2>
-            </div>
-            {/* skill show section */}
-            <div
-                className="text-xs text-center font-bold mt-3 mb-8 px-4">
-                <div
-                    className="flex items-center justify-center mb-3">
-                    <p
-                        className="bg-zinc-200 rounded-full mx-1 items-center justify-center flex w-fit px-7 py-3">
-                        {skills?.skillOne}<GiTrophy className="text-yellow-500 text-base ms-1" />
-                    </p>
-                    <p
-                        className="bg-zinc-200 rounded-full mx-1 py-3 items-center justify-center flex w-fit px-7">
-                        {skills?.skillTwo}
-                    </p>
-                </div>
-                <div
-                    className="flex items-center justify-center">
-                    <p
-                        className="bg-zinc-200 rounded-full  mx-1 py-3 items-center justify-center flex w-fit px-7">
-                        {skills?.skillThree}
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="card border  border-gray-200 bg-base-100 shadow-sm mx-3 my-2">
+      <div className="mt-5 mx-3 flex justify-end">
+        <h2 className="justify-center px-6 py-1 font-light bg-yellow-400 rounded-full text-white text-xs w-[35%] lg:w-[28%] text-center">
+          {seekerJobTime}
+        </h2>
+      </div>
+      <figure>
+        <Image
+          width={160}
+          height={160}
+          src={image}
+          className="rounded-full my-3"
+          alt="Seekers Image"
+        />
+      </figure>
+      <div>
+        <h2 className="text-center font-bold my-1 text-xl">{seekerName}</h2>
+        <p className="text-center font-medium text-sm">{findingJobTitle}</p>
+        <p className="text-center font-medium text-sm py-1">
+          Technical: {education}
+        </p>
+      </div>
+      <div className="mb-2">
+        <h2 className="text-center font-bold">
+          Experience: {jobExperience} <span className="text-xs">Year</span>
+          <PiShootingStarFill className="text-xl text-yellow-400 ms-1" />
+        </h2>
+      </div>
+      {/* skill show section */}
+      <div className="text-xs text-center font-bold mt-3 mb-8 px-4">
+        <div className="flex items-center justify-center mb-3">
+          <p className="bg-zinc-200 rounded-full mx-1 items-center justify-center flex w-fit px-7 py-3">
+            {skills?.skillOne}
+            <GiTrophy className="text-yellow-500 text-base ms-1" />
+          </p>
+          <p className="bg-zinc-200 rounded-full mx-1 py-3 items-center justify-center flex w-fit px-7">
+            {skills?.skillTwo}
+          </p>
         </div>
-    );
+        <div className="flex items-center justify-center">
+          <p className="bg-zinc-200 rounded-full  mx-1 py-3 items-center justify-center flex w-fit px-7">
+            {skills?.skillThree}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleJobSeekers;
