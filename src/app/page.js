1e409f91f16dc49ac2b07ce2e606f7057f64fa7e
SQLAdmin <abuyeahia24@gmail.com>
2023-08-23 15:39:47 +0600

    added ecnoding packge

--- src/app/page.js
+++ src/app/page.js
@@ -11,26 +11,49 @@ import TestimonialSection from "@/components/HomePage/TestimonialSection/Testimo
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Blog from "@/components/HomePage/Blog/Blog";
+import FeaturedJobs from "@/components/HomePage/FeaturedJobs/FeaturedJobs";
 
 
 
 export default function Home() {
   useEffect(() => {
     AOS.init({
-         duration: 800,
-         once: false,
-       })
- }, [])
+      duration: 800,
+      once: false,
+    })
+  }, [])
 
 
   return (
     <div>
-    
-      {/* Banner Section */}
+
+      {/* Banner Section /} */}
+
       <HomeBanner></HomeBanner>
 
-      {/* Popular job category section */}
+      {/* {/ Popular job category section /} */}
       <JobCategories></JobCategories>
+
+      {/* {/ feature job section /} */}
+      <FeaturedJobs></FeaturedJobs>
+
+      {/* {/ Seeker Post Section /} */}
+      <JobSeekers></JobSeekers>
+
+      {/* {/ Jobs Banner section /} */}
+      <JobsBanner></JobsBanner>
+
+      {/* {/SponsorShip section /} */}
+      <SponsorShip></SponsorShip>
+
+      {/* {/ Jobs Blog section /} */}
+
+      <Blog></Blog>
+
+      {/* {/ Testimonial section */}
+      <TestimonialSection></TestimonialSection>
+
     </div>
   );
 }
