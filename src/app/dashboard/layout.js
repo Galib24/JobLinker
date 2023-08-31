0d133ed0d02cdcb9eb2b8f82948107338df62129
SQLAdmin <abuyeahia24@gmail.com>
2023-08-31 17:17:01 +0600

    added dashboard setup

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,8 +1,37 @@
+import Link from "next/link";
+
 const DashboardLayout = ({ children }) => {
+  const isAdmin = true;
+  const isHr = false;
   return (
-    <div className="flex">
-      <h2 className="w-1/4">SideBar</h2>
-      <div className="w-3/4">{children}</div>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Page content here */}
+        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        {children}
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {/* Sidebar content here */}
+          <li><a>Sidebar Item 1</a></li>
+          <li><Link href={`/`}>Home</Link></li>
+          {
+            isAdmin && <>
+              <li><Link href={`/dashboard/users`}>Sidebar Item admin item 1</Link></li>
+              <li><a>Sidebar Item admin item 2</a></li>
+            </>
+          }
+          {
+            isHr && <>
+              <li><a>Sidebar Item HR item 1</a></li>
+              <li><a>Sidebar Item Hr item 2</a></li>
+            </>
+          }
+        </ul>
+
+      </div>
     </div>
   );
 };
