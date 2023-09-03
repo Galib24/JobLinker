2baa7431119e07540722c8ea2f1c610e626a6d88
SQLAdmin <abuyeahia24@gmail.com>
2023-09-03 12:56:56 +0600

    added some routes change

---
+++ src/app/dashboard/(admin-route)/analysis/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnalysisPage = () => {
+    return (
+        <div>
+            <h1>analysis page</h1>
+        </div>
+    );
+};
+
+export default AnalysisPage;
---
+++ src/app/dashboard/(hr-routes)/myjobpost/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const hrJobPostPage = () => {
+    return (
+        <div>
+            <h2>jr job post pages</h2>
+        </div>
+    );
+};
+
+export default hrJobPostPage;
