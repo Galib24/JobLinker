a53d4076562b74a602da419ef0c44dace30ec1e1
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-09-04 18:07:40 +0600

    admin power checked

--- src/app/dashboard/(admin-route)/users/page.js
+++ src/app/dashboard/(admin-route)/users/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 // import { useEffect, useState } from "react";
@@ -6,108 +6,117 @@ import { FaTrashAlt } from "react-icons/fa";
 import { useQuery } from "react-query";
 
 const UsersPage = () => {
-    // const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
-    // get the data
-    // useEffect(() => {
-    //     const fetchUsersData = async () => {
-    //         const response = await fetch("/api/users");
-    //         const data = await response.json();
-    //         console.log(data);
-    //         setUsers(data);
-    //     };
+  // get the data
+  // useEffect(() => {
+  //     const fetchUsersData = async () => {
+  //         const response = await fetch("/api/users");
+  //         const data = await response.json();
+  //         console.log(data);
+  //         setUsers(data);
+  //     };
 
-    //     fetchUsersData();
-    // }, []);
+  //     fetchUsersData();
+  // }, []);
 
+  // fix this
 
-    // fix this
+  const {
+    data: users = [],
+    refetch,
+    isLoading,
+    error,
+  } = useQuery({
+    queryFn: async () => {
+      const res = await axios("/api/users");
+      return res.data;
+    },
+  });
 
-
-    const {
-        data: users = [],
-        refetch,
-        isLoading,
-        error,
-    } = useQuery({
-        queryFn: async () => {
-            const res = await axios("/api/users");
-            return res.data;
-        },
-    });
-
-    if (isLoading)
-        return (
-            <h2 className="text-4xl font-semibold text-green-700 text-center mt-10">Loading...</h2>
-        );
-
-    // add admin handle
-
-    const handleMakeAdmin = async (user) => {
-        const updateUserData = {
-            name: user.name,
-            email: user.email,
-            photo: user.photo ? user.photo : " ",
-            role: "admin"
-
-        }
-        const res = await fetch(`/api/users/${user._id}`, {
-            method: 'PUT',
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify(updateUserData)
-        })
-        console.log(res);
-        if (res.ok) {
-            refetch();
-            toast.success(`${user.name} is now Admin`)
-        }
-    }
-
-
+  if (isLoading)
     return (
-        <>
-            <h3 className="text-3xl font-semibold text-center my-4">Total Users: {users.length}</h3>
-            <div className="overflow-x-auto">
-                <table className="table table-zebra">
-                    {/* head */}
-                    <thead className="text-center">
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Power</th>
-                            <th>Instructor</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user, i) => <tr
-                                key={user._id}
-                                className="text-center"
-                            >
-                                <th>{i + 1}</th>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.role === 'admin' ? 'Admin' : <button onClick={() => handleMakeAdmin(user)} className="btn text-blue-500 btn-ghost btn-md">Make Admin</button>}</td>
-
-                                <td><button className="btn text-blue-500 btn-ghost btn-md">user</button></td>
-                                <td>
-
-                                    <button className="btn text-red-500 btn-ghost btn-md"><FaTrashAlt></FaTrashAlt></button>
-
-                                </td>
-                            </tr>)
-                        }
-
-                    </tbody>
-                </table>
-            </div>
-
-        </>
+      <h2 className="text-4xl font-semibold text-green-700 text-center mt-10">
+        Loading...
+      </h2>
     );
+
+  // make admin handle
+  const handleMakeAdmin = async (user) => {
+    const updateUserData = {
+      name: user.name,
+      email: user.email,
+      photo: user.photo ? user.photo : "",
+      role: "admin",
+    };
+    const res = await fetch(`/api/users/${user._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateUserData),
+    });
+    console.log(res);
+    if (res.ok) {
+      refetch();
+      toast.success(`${user.name} is now Admin`);
+    }
+  };
+
+  return (
+    <>
+      <h3 className="text-3xl font-semibold text-center my-4">
+        Total Users: {users.length}
+      </h3>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead className="text-center">
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Power</th>
+              <th>Instructor</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={user._id} className="text-center">
+                <th>{i + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn text-blue-500 btn-ghost btn-md"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+
+                <td>
+                  <button className="btn text-blue-500 btn-ghost btn-md">
+                    user
+                  </button>
+                </td>
+                <td>
+                  <button className="btn text-red-500 btn-ghost btn-md">
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default UsersPage;
