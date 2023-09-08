21af571d845b649cc9845168926fb50a84ef56d9
SQLAdmin <abuyeahia24@gmail.com>
2023-09-08 17:16:07 +0600

    added some packg

--- _tests_/HomePage.test.jsx
+++ _tests_/HomePage.test.jsx
@@ -1,1 +1,13 @@
-describe('home page')
+import HomeBanner from '@/components/HomePage/Banner/HomeBanner';
+import { render, screen, waitFor } from '@testing-library/react';
+
+describe('Home Page - Rendering', () => {
+  it('should have Join us text in banner', async () => {
+    render(<HomeBanner />);
+    
+    // Wait for the text to appear asynchronously
+    await waitFor(() => {
+      expect(screen.getByText('Join us')).toBeInTheDocument();
+    });
+  });
+});
