a5126252e0b5302573ec5cffd3e6fbfb36341d69
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-09-15 18:00:02 +0600

    make responsive by shakil

--- src/app/(with-navbar)/blogs/page.js
+++ src/app/(with-navbar)/blogs/page.js
@@ -96,12 +96,163 @@ const BlogsPage = () => {
         <div className="bg-indigo-50">
           <BannerComponent headingTitle="Blog Post" pageName="Blog" />
         </div>
+
+         {/* others */}
+         <div className="col-span-4 mx-4 mt-10 lg:hidden block">
+            <div className="flex gap-2 items-center pb-3 cursor-pointer">
+              <Image
+                width={50}
+                height={50}
+                src={user?.photoURL || defaultImg}
+                className="rounded-full"
+                alt=""
+              />
+              <p
+                onClick={() =>
+                  document.getElementById("my_modal_8").showModal()
+                }
+                className="px-6 py-3 bg-indigo-50 w-full rounded-full font-semibold text-gray-500 flex items-center gap-2"
+              >
+                <FiEdit className="text-xl" /> Write A Blog
+              </p>
+            </div>
+
+            {/* modal */}
+            <div>
+              <dialog id="my_modal_8" className="modal">
+                <div className="modal-box">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                      ✕
+                    </button>
+                  </form>
+                  {/* text editor */}
+                  <h2 className="text-2xl text-center font-semibold my-2">
+                    Write A Blog
+                  </h2>
+                  <hr />
+                  <div>
+                    {/* img name */}
+                    <div className="flex items-center gap-2 my-3">
+                      <Image
+                        width={35}
+                        height={35}
+                        src={user?.photoURL || defaultImg}
+                        className="rounded-full"
+                        alt=""
+                      />
+
+                      <p className="font-semibold text-lg">
+                        {user?.displayName || "John Doe"}
+                      </p>
+                    </div>
+                    {/* img name */}
+                  </div>
+
+                  {/* react hook form for getting data */}
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* Blog Title Field */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text font-semibold">
+                          Blog Title*
+                        </span>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Blog Title"
+                        {...register("blogTitle", {
+                          required: true,
+                        })}
+                        className="w-full input input-bordered uppercase"
+                      />
+                    </div>
+
+                    {/* Blog Photo */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text font-semibold">
+                          Blog Photo*
+                        </span>
+                      </label>
+                      <input
+                        type="file"
+                        placeholder="Blog Photo"
+                        {...register("blogPhoto", {
+                          required: true,
+                        })}
+                        className="w-full file-input file-input-bordered"
+                      />
+                    </div>
+
+                    {/* Blog description Field */}
+                    <div className="form-control flex flex-col mt-2">
+                      <label className="label">
+                        <span className="label-text font-semibold">
+                          Job Description*
+                        </span>
+                      </label>
+                      <textarea
+                        id=""
+                        cols="30"
+                        rows="4"
+                        placeholder="Blog Description"
+                        {...register("blogDetails", { required: true })}
+                        className="textarea textarea-bordered textarea-md w-full "
+                      ></textarea>
+                    </div>
+
+                    {/* Submit Button */}
+                    <div className="text-center my-10">
+                      <input
+                        className="btn bg-[#40e1f9] px-10 p-2 rounded-lg text-white hover:bg-transparent hover:text-[#40e1f9] hover:font-extrabold hover:border-y-2 hover:border-x-2 hover:border-[#40e1f9] ease-out duration-300"
+                        type="submit"
+                        value="Post Blog"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </dialog>
+            </div>
+            {/* contact Div */}
+            <div className="bg-indigo-50 rounded-xl px-7 py-3  mb-7 text-xl mt-8 hidden lg:block">
+              <h2 className="text-xl font-semibold mt-4">Contact Us</h2>
+              {/* form */}
+              <div className="mx-auto mt-5 mb-3">
+                {/* name */}
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="input input-bordered w-full py-8"
+                />
+                {/* email */}
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="input input-bordered w-full my-3 py-8"
+                />
+                {/* text area */}
+                <textarea
+                  className="textarea textarea-bordered w-full h-40"
+                  placeholder="Message"
+                ></textarea>
+
+                <button className="btn bg-[#40e1f9] px-10 p-3 rounded-lg text-white hover:bg-transparent hover:text-[#40e1f9] hover:font-extrabold hover:border-y-2 hover:border-x-2 hover:border-[#40e1f9] ease-out duration-300 mt-3 w-full">
+                  Send Message
+                </button>
+              </div>
+              {/* form */}
+            </div>
+            {/* contact Div */}
+          </div>
+          {/* others */}
         {/* header div */}
 
         {/* body */}
         <div className="w-[95%] mx-auto grid grid-cols-12 mt-14 mb-20">
           {/* card */}
-          <div className="col-span-8 mx-6">
+          <div className="col-span-12 lg:col-span-8 mx-6">
             {/* card container */}
 
             {
@@ -115,7 +266,7 @@ const BlogsPage = () => {
           {/* card */}
 
           {/* others */}
-          <div className="w-full col-span-4 mx-4">
+          <div className="w-full col-span-4 mx-4 hidden lg:block">
             <div className="flex gap-2 items-center pb-3 cursor-pointer">
               <Image
                 width={50}
@@ -233,10 +384,10 @@ const BlogsPage = () => {
               </dialog>
             </div>
             {/* contact Div */}
-            <div className="bg-indigo-50 rounded-xl px-7 py-3  mb-7 text-xl mt-8">
+            <div className="bg-indigo-50 rounded-xl px-7 py-3  mb-7 text-xl mt-8 hidden lg:block">
               <h2 className="text-xl font-semibold mt-4">Contact Us</h2>
               {/* form */}
-              <div className="mx-auto mt-5 mb-3 ">
+              <div className="mx-auto mt-5 mb-3">
                 {/* name */}
                 <input
                   type="text"
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,40 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import loginLottie from "../../../public/DashboardWelcome.json";
+import Lottie from "lottie-react";
+import { useContext } from "react";
+import { AuthContext } from "@/provider/AuthProvider";
+
 const DashboardPage = () => {
+
+  const { user } = useContext(AuthContext);
+
   return <div>
-    {/* <DashBoardHome></DashBoardHome> */}
-    <h2>Dashboard</h2>
+    <Link
+      href="/"
+      className="flex items-center justify-center">
+      <Image
+        width={190}
+        height={190}
+        className="cursor-pointer rounded-md"
+        src="https://i.ibb.co/ZMxRfGL/job-Linker-Logo.png"
+        alt="" />
+    </Link>
+    <div>
+      <Lottie
+        animationData={loginLottie}
+        loop={true}
+        className="mx-auto mt-12" />
+      <br />
+      <div>
+        <h2
+          className="text-center font-semibold text-2xl text-gray-600">
+          {user?.displayName || "Anonymous"} This Is Your Dashboard
+        </h2>
+      </div>
+    </div>
   </div>;
 };
 
